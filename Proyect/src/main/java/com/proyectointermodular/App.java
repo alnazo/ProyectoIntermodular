90b25148696085edf67cd28c95042778e6988308
Antonio <a2llamas@gmail.com>
2023-04-22 17:54:07 +0200

    Add css to test template

--- Proyect/src/main/java/com/proyectointermodular/App.java
+++ Proyect/src/main/java/com/proyectointermodular/App.java
@@ -15,6 +15,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("view/primary"), 640, 480);
+        scene.getStylesheets().add(getClass().getResource("css/styles.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
