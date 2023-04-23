ec9ac21f9a9fd33100c76e91b833476eb598c5aa
Antonio <a2llamas@gmail.com>
2023-04-23 02:29:45 +0200

    Arranque de aplicacion eliminando el module-info.java

--- Proyect/src/main/java/com/proyectointermodular/App.java
+++ Proyect/src/main/java/com/proyectointermodular/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("view/primary"), 640, 480);
-        scene.getStylesheets().add(getClass().getResource("css/styles.css").toExternalForm());
+        scene.getStylesheets().add("com/proyectointermodular/css/stiles.css");
         stage.setScene(scene);
         stage.show();
     }
